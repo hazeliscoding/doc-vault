dd4be6fc2c082fd9b33a0d2d4f7d01200f82184b
ducksoop <torresj1997@outlook.com>
2024-02-19 10:48:57 -0600

    test: 💍 google sign-in and drive upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public GOOGLE_CLIENT_ID =
+    '1028208457453-mm7ghkd501pj27r0qs4c2fat9715mmgi.apps.googleusercontent.com';
 }
---
+++ src/app/file-upload/file-upload.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { GoogleDriveAuthService } from './google-drive-auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileUploadService {
+  constructor(private authService: GoogleDriveAuthService) {}
+
+  upload(file: File) {
+    console.log(`Uploading file: ${file.name} to Google Drive...`);
+    this.authService
+      .auth()
+      .then(() => {
+        console.log('TODO');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
