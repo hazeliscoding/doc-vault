bfbf4eae2731ca937ecc3f70856aad3b51a522fb
ducksoop <torresj1997@outlook.com>
2024-02-19 09:49:23 -0600

    feat: 🎸 added file selection with native file input

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
-  styleUrls: ['./file-upload.component.scss']
+  styleUrls: ['./file-upload.component.scss'],
 })
 export class FileUploadComponent {
+  fileName = '';
 
+  onFileSelected(event: Event) {
+    let file: File;
+    if (event.target) {
+      const target = event.target as HTMLInputElement;
+      if (target.files) {
+        file = target.files[0];
+        this.fileName = file.name;
+      }
+    }
+  }
 }
